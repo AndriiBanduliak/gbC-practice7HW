827ba68187ae4ddef893dc5746b7826380a1bec7
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-28 14:15:20 +0300

    3rd task is done

--- Program.cs
+++ Program.cs
@@ -1,63 +1,44 @@
-﻿//Задача 50.Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+﻿//Задача 52.Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
 //Например, задан массив:
-
 //1 4 7 2
-
 //5 9 2 3
-
 //8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-//17->такого числа в массиве нет
 
 using static System.Console;
 Clear();
 
-
-Write("Rows in an array:");
-int m=int.Parse(ReadLine());
-Write("Columns in an array:");
-int n = int.Parse(ReadLine());
-double[,]arr=new double[m,n];
-Write("What the number to find in the array:");
-int digit=int.Parse(ReadLine());
-fillArr(arr);
-printArr(arr);
-printArrDigit(arr,digit);
-
-void fillArr(double[,]arr)
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
+int[,] arr = new int[,]
     {
-        for(int j = 0; j < arr.GetLength(1); j++)
-        {
-            Random x = new Random();
-            arr[i, j] = x.Next(10);
+    {1,4,7,2},
+    {5,9,2,3},
+    {8,4,2,4}
+    };
 
+
+printColAverage(arr);
+
+
+void printColAverage(int[,]arr)
+{
+    Write("Arithmetic mean of each column: ");
+    int[,] revertArr = new int[arr.GetLength(1), arr.GetLength(0)];
+    for (int i = 0; i < revertArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < revertArr.GetLength(1); j++)
+        {
+            revertArr[i,j] = arr[j,i];
         }
     }
-}
-void printArr(double[,] arr)
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
+    double[] counter=new double[revertArr.GetLength(0)];
+    for (int i = 0; i < revertArr.GetLength(0); i++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        for (int j = 0; j < revertArr.GetLength(1); j++)
         {
-            Write($"{arr[i, j]} ");
+            counter[i] += revertArr[i, j];
         }
-        Write("\n");
+        Write($"{Math.Round(counter[i]/ revertArr.GetLength(1),2)} ");
     }
-}
-void printArrDigit(double[,] arr,int digit)
-{
-    int checkCounter = 0;
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            if (arr[i, j] == digit) WriteLine($"The number {digit} found in the  {i + 1} row and in the {j + 1} collumn");
-            else checkCounter++;
-        }
-    }
-    if (checkCounter == arr.Length) WriteLine("There is no such number");
 }
